4bee69c59682d237eea14a44dee6c80a58a05199
Mufti Ramdhani <muftiramdhani25@gmail.com>
2022-04-21 18:36:28 +0300

    create dialog fragment

--- app/src/main/java/com/mufti/binarandroid/MainActivity.kt
+++ app/src/main/java/com/mufti/binarandroid/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mufti.binarandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDialogFragment: Button = findViewById(R.id.btn_fragment_dialog)
+
+        btnDialogFragment.setOnClickListener {
+            val dialogFragment = CustomDialogFragment()
+            dialogFragment.show(supportFragmentManager, null)
+        }
     }
 }
